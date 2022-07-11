ffd628dcf7f029c1400548f6c20b3dd9fd545cea
Marcela Mejia <mejiao24@hotmail.com>
2022-07-11 16:59:59 +1000

    Render city weather

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,10 +5,7 @@ function init () {
 
         // Search city and get the coordinates
         var city = $("#input-city").val();
-        var promise = convertCityToCoordinates(city);
-        promise.then(function(coordinates){
-            console.log(coordinates);
-        });
+        renderWeather(city);
         
         // Get the city's weather
         // Render the city's weather on the screen
@@ -31,21 +28,42 @@ function saveState() {
     localStorage.setItem("weather_dashboard", json);
 }
 
-function convertCityToCoordinates(city)
-{
-    return fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(city) + '&key=AIzaSyBWkMk0SZcUUxPMUw__hBkOQRaahoZMZHo')
+function renderWeather(city) {
+    // get the coordinates of the city
+    fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(city) + '&key=AIzaSyBWkMk0SZcUUxPMUw__hBkOQRaahoZMZHo')
         .then(response => response.json())
         .then(data => {
             // Gets the city longitude and latitude coordinates
-            var lat = data.results[0].geometry.location.lat;
-            var lng = data.results[0].geometry.location.lng;
+            var latitude = data.results[0].geometry.location.lat;
+            var longitude = data.results[0].geometry.location.lng;
 
             var coordinates = {
-                latitude: lat,
-                longitude: lng
+                latitude: latitude,
+                longitude: longitude
             };
             return coordinates;           
-        });
+        })
+        .then(function(coordinates) {
+            // with the coordinates of the city obtain the weather
+            fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + coordinates.latitude + "&lon=" + coordinates.longitude +"&units=metric&appid=d7b05bd3309734b1667a4dcf67a3c01a")
+            .then(response => response.json())
+            .then(function(weather){
+                return weather;
+            })
+            .then(function(weather){
+                // Render the weather
+                var today = new Date();
+                var todayString = ' (' +today.getDate() + '/' + (today.getMonth() + 1) + '/' + today.getFullYear() + ')';
+
+                $("#city").text(city + todayString);
+                $("#temperature").text(weather.current.temp + "°C");
+                $("#wind").text(weather.current.wind_speed + " km/h");
+                $("#humidity").text(weather.current.humidity + " %");
+                $("#uv-index").text(weather.current.uvi);
+                
+            });
+        })
+        ;
 }
 
 init();
