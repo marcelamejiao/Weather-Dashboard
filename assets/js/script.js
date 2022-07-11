00f5c95c4d7f2707243f2d5858d40a597465d130
Marcela Mejia <mejiao24@hotmail.com>
2022-07-11 15:14:14 +1000

    Implement function to get coordinates (lat and lng)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,20 @@
 var state = {};
 
 function init () {
+    $("#search-button").on('click',function(){
+
+        // Search city and get the coordinates
+        var city = $("#input-city").val();
+        var promise = convertCityToCoordinates(city);
+        promise.then(function(coordinates){
+            console.log(coordinates);
+        });
+        
+        // Get the city's weather
+        // Render the city's weather on the screen
+        // Render the 5 day forecast 
+        // Render search history
+    });
 }
 
 function loadState() {
@@ -17,19 +31,21 @@ function saveState() {
     localStorage.setItem("weather_dashboard", json);
 }
 
-// $("#search-button").on('click',function(){
+function convertCityToCoordinates(city)
+{
+    return fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(city) + '&key=AIzaSyBWkMk0SZcUUxPMUw__hBkOQRaahoZMZHo')
+        .then(response => response.json())
+        .then(data => {
+            // Gets the city longitude and latitude coordinates
+            var lat = data.results[0].geometry.location.lat;
+            var lng = data.results[0].geometry.location.lng;
 
-// });
-
-// function convertAddressToCoordinates(address)
-// {
-//     fetch("https://maps.googleapis.com/maps/api/geocode/json?address=" + encodeURIComponent(address) + '&key=AIzaSyBWkMk0SZcUUxPMUw__hBkOQRaahoZMZHo')
-//     .then(response => response.json())
-//     .then(data => {
-//         // Gets the address longitude and latitude coordinates
-//         var lat = data.results[0].geometry.location.lat;
-//         var lng = data.results[0].geometry.location.lng;
-//     })
-// }
+            var coordinates = {
+                latitude: lat,
+                longitude: lng
+            };
+            return coordinates;           
+        });
+}
 
 init();
